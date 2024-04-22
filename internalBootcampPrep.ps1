1161395d976146ba26f93bf177c34b6101425dbf
davijenn <98111015+davijenn@users.noreply.github.com>
2024-04-22 07:57:17 -0400

    Update internalBootcampPrep.ps1

--- internalBootcampPrep.ps1
+++ internalBootcampPrep.ps1
@@ -57,7 +57,7 @@ $null = Read-Host -Prompt "Press Enter to continue"
 # Now we'll connect to MgGraph, AzureAD and MS Teams. New error logic added to handle authentication failure
 
 try {
-    Connect-MgGraph -Scopes "User.ReadWrite.All,Policy.ReadWrite.ConditionalAccess, Policy.Read.All, Domain.ReadWrite.All" -NoWelcome -ErrorAction Stop
+    Connect-MgGraph -Scopes "User.ReadWrite.All,Policy.ReadWrite.ConditionalAccess, Policy.Read.All, Domain.ReadWrite.All, AppRoleAssignment.ReadWrite.All" -NoWelcome -ErrorAction Stop
 
 } catch {
     Write-Host "
